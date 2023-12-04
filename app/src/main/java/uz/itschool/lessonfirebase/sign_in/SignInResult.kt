227ab39928423ead6ced31bf1f2216f501ee5017
User_7 <khabibullokh2006@gmail.com>
2023-12-04 14:35:50 +0500

    Initial commit

---
+++ app/src/main/java/uz/itschool/lessonfirebase/sign_in/SignInResult.kt
@@ -0,0 +1,14 @@
+package uz.itschool.lessonfirebase.sign_in
+
+
+
+data class SignInResult (
+    val data : UserData?,
+    val errorMessage: String?
+)
+
+data class UserData(
+    val userId:String,
+    val username:String?,
+    var profilePictureUrl:String?
+)
